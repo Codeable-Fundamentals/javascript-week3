538a610329d8d83a84547e3e067af7477b9f8196
Grecia Delgado <greciadm2596@gmail.com>
2025-07-10 18:40:52 -0600

    feat: update exmaples of HOF 2

---
+++ High-Order-Functions/teoria-HOF-2.js
@@ -0,0 +1,12 @@
+// que una HOF es aquella que deveulva o retorna otra funcion.
+
+function saludar() {
+  const hola = () => "hola soy Grecia!";
+
+  return hola;
+}
+
+const res = saludar();
+
+console.log("Sin (): ",res)
+console.log("Con (): ",res())
