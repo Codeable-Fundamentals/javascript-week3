d07d958492ffee9661d28bffa7964396251dd6fd
Grecia Delgado <greciadm2596@gmail.com>
2025-07-10 19:26:46 -0600

    Update examples

---
+++ High-Order-Functions/ejercicios/reduce.js
@@ -0,0 +1,6 @@
+let arr = [1, 2, 3, 4, 5];
+// factorial !5 = 1 x 2 x3 x4 x 5
+
+const resp = arr.reduce((a, b) => a * b, 1);
+
+console.log(resp);
---
+++ High-Order-Functions/ejercicios/reverse.js
@@ -0,0 +1,5 @@
+const arr = [8, 4, 5, 7, 1];
+
+let resp= arr.reverse()
+
+console.log(resp)
