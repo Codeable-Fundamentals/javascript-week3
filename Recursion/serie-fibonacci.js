96f545e0f2455d5471b1c2366d9d181898eb6590
Grecia Delgado <greciadm2596@gmail.com>
2025-07-21 18:25:28 -0600

    upodate example

--- Recursion/serie-fibonacci.js
+++ Recursion/serie-fibonacci.js
@@ -1,4 +1,10 @@
 // serie fibonacci
 // 0,1,1,2,3,5,8,13,21,34,....
 
+// La funcion espera que devuelvas un numero entero
+// ejemplos:
+// fibonacci(5) => 5
+// fibonacci(8) => 21
+// fibonacci(0) => 0
+// fibonacci(1)=> 1 
 function fibonacci(n) {}
