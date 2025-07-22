555127fa6e895c72f69d439b43013d2a92c6bbbf
Grecia Delgado <greciadm2596@gmail.com>
2025-07-21 19:33:32 -0600

    add example

--- Async/ejmeplo-tricky.js
+++ Async/ejmeplo-tricky.js
@@ -1,7 +1,15 @@
+setTimeout(() => console.log("Tarea programada!!!"), 0);
+
+function counter(n) {
+  for (let i = 0; i < n; i++) {
+    console.log("numero : ", i);
+  }
+}
+
 console.log("Antes de la planificación");
- 
-setTimeout(() => console.log("Tarea programada"), 0)
- 
+
 console.log("Después de la planificación");
- 
+
 console.log("Mucho después de la planificación");
+
+counter(1000);
