b2966014574a7370c4553264e52e0a624edac3c7
Grecia Delgado <greciadm2596@gmail.com>
2025-07-22 19:06:10 -0600

    Update example

--- ErrorHandling/teoria/async-error-handling-good.js
+++ ErrorHandling/teoria/async-error-handling-good.js
@@ -3,6 +3,9 @@ setTimeout(() => {
   try {
     console.log(saludo);
   } catch (error) {
+    console.log("Error message 💬: ", error.message);
+    console.log("Error name 🧔: ", error.name)
+    console.log("Error stack : ", error.stack)
     console.log("Hubo un error 🫢 !");
   }
 }, 1000);
