215d4e5b3f38895f3108ffc339f3fd161d553ce5
Grecia Delgado <greciadm2596@gmail.com>
2025-07-22 18:36:04 -0600

    update ErrorHandling/teoria/sin-manejo-de-error.js

---
+++ ErrorHandling/teoria/sin-manejo-de-error.js
@@ -0,0 +1,7 @@
+console.log("Antes del saludo");
+
+console.log(saludo); // ReferenceError: saludo is not defined
+
+// El programa "se muere", el siguiente console.log nunca se ejecuta
+
+console.log("Después del saludo");
