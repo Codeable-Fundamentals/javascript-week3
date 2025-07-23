d518cc52b10d89709fc1628da3e3d7537e56a735
Grecia Delgado <greciadm2596@gmail.com>
2025-07-22 19:00:03 -0600

    update exmaples

--- ErrorHandling/teoria/async-error-handling-bad.js
+++ ErrorHandling/teoria/async-error-handling-bad.js
@@ -1,7 +1,7 @@
 console.log("Antes del saludo");
 
 try {
-  setTimeout(() => console.log(saludo), 1000); // <-- lanzará un error
+  setTimeout(() => console.log(saludo), 1000); // <-- lanzará un error // WEB API MACROTASK Queue (cola de MACRO TAREAS )
 } catch (error) {
   // no entro aqui :s
   console.error("Se detectó un error 🫢🫢🫢🫢🫢");
