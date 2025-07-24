f6a8e79463235c42ec2c8170a37e19d54840edb0
Grecia Delgado <greciadm2596@gmail.com>
2025-07-23 20:04:37 -0600

    Update example

--- AsyncClase/teoria/ejercicio-login.js
+++ AsyncClase/teoria/ejercicio-login.js
@@ -13,6 +13,54 @@ const users = [
   },
 ];
 
+// Simulamos una base de datos de repositorios
+const githubPublicRepos = [
+  { id: 1, owner: "pepe-20", name: "hello-world" },
+  { id: 2, owner: "pepe-20", name: "say-hi" },
+  { id: 3, owner: "anita-dev", name: "first-repo" },
+  { id: 4, owner: "anita-dev", name: "codeable" },
+  { id: 5, owner: "anita-dev", name: "example" },
+];
+
+function getPublicRepos(userName, callbackFn) {
+  // Simulando la conexion con Gitub
+  const hasConnectionError = Math.random() > 0.5;
+  if (hasConnectionError) {
+    throw new Error("Error con la conexion de Github!");
+  }
+
+  setTimeout(() => {
+    const reposFounded = githubPublicRepos.filter(
+      (repo) => repo.owner === userName
+    );
+    if (reposFounded.length === 0) {
+      const error = new Error("El usuario, no tiene repositorios");
+      callbackFn(error);
+      return;
+    }
+
+    callbackFn(null, reposFounded);
+  }, 2000);
+}
+
+const getRepos = (error, listOfRepos) => {
+  if (error) {
+    console.log(error.message);
+    return;
+  }
+
+  console.log("Lista de repos!!!: ", listOfRepos);
+  return listOfRepos;
+};
+
+try {
+  const test = getPublicRepos("anita-dev", getRepos);
+  console.log("TEST REPOS ", test); // undefined
+} catch (error) {
+  console.log(error.message);
+}
+
+console.log("-----------------------------");
 // se encargara de devolver un usuario
 function getUser(email, password, callback) {
   if (typeof email !== "string" || typeof password !== "string") {
@@ -43,7 +91,7 @@ function getUser(email, password, callback) {
 
 const cualquierNombre = (error, user) => {
   console.log("Me llega el error ? : ", error);
-  console.log("Me llega el user ????: ", user)
+  console.log("Me llega el user ????: ", user);
   if (error) {
     console.error(error.message);
     return;
