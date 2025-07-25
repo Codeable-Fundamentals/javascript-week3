700aa44f5524cd2b34cf4f065b1db07e5242ef02
Grecia Delgado <greciadm2596@gmail.com>
2025-07-24 19:44:18 -0600

    Update examples

--- Promises/teoria/teoria.js
+++ Promises/teoria/teoria.js
@@ -3,13 +3,13 @@
 
 // Ejemplo de una promesa, cuando todo sale bien
 let promise = new Promise(function (resolve, reject) {
-  setTimeout(() => resolve("done"), 1000);
+  setTimeout(() => resolve("done!!!!!!!!"), 1000);
 });
 
 // ejemplod de una promesa que salio mal
-// let promise_error = new Promise(function (resolve, reject) {
-//   setTimeout(() => reject(new Error("algo salio mal :c ")), 1000);
-// });
+let promise_error = new Promise(function (resolve, reject) {
+  setTimeout(() => reject(new Error("algo salio mal :c ")), 1000);
+});
 
 console.log(promise);
 console.log(promise.status);
@@ -17,3 +17,23 @@ console.log(promise.__proto__);
 console.log(Object.getPrototypeOf(promise));
 
 console.log(promise.result);
+
+promise.then(
+  function (result) {
+    /*Caso exito */
+    console.log("Resultado de la promesa: ", result);
+  },
+  function (error) {
+    /* Caso rechazo */
+    console.log("Resultado erroneo de la promesa: ", error);
+  }
+);
+
+promise_error.then(
+  function (result) {
+    console.log("Resultado exitoso: ", result);
+  },
+  function (error) {
+    console.log("Resultado erroneo: ", error);
+  }
+);
