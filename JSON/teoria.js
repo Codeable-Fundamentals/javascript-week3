22e4bb8203864a3f57ca731191a22e89a2d6f84a
Grecia Delgado <greciadm2596@gmail.com>
2025-07-25 19:55:32 -0600

    update JSON examples

---
+++ JSON/teoria.js
@@ -0,0 +1,15 @@
+// JSON:
+// JavaScript Object Notation
+
+// ¿Que es ?
+// es un formato , para intercamabair datos
+// lo identificamos por que sus files , terminan en la extension .json
+
+const escuela = {
+  nombre: "Codeable",
+  fundacion: 2019
+}
+ 
+console.log("Escuela: " + escuela); // Escuela: [object Object]
+console.log("Escuela: " + escuela.nombre); // Escuela: [object Object]
+console.log("Escuela: " + escuela.fundacion); // Escuela: [object Object]
