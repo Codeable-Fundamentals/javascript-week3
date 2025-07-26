a475b6a0917eabf1b6d46154d4417b9e5a198015
Grecia Delgado <greciadm2596@gmail.com>
2025-07-25 20:05:20 -0600

    update examples

--- JSON/teoria.js
+++ JSON/teoria.js
@@ -13,3 +13,12 @@ const escuela = {
 console.log("Escuela: " + escuela); // Escuela: [object Object]
 console.log("Escuela: " + escuela.nombre); // Escuela: [object Object]
 console.log("Escuela: " + escuela.fundacion); // Escuela: [object Object]
+
+console.log("Antes del JSON.stringify: ", typeof escuela)
+const test = JSON.stringify(escuela)
+console.log("que hace JSON.stringify ?: ",test )
+console.log("que hace JSON.stringify ?: ",typeof test )
+
+const test_2 = JSON.parse(test)
+console.log("que hace JSON.parse ?: ",test_2 )
+console.log("que hace JSON.parse ?: ",typeof test_2 )
